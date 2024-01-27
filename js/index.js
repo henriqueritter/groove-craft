ed08ec719d98a16608a0b7dd7c4c8b7d3e4f7cd1
henriqueritter <henrique-ritter@hotmail.com>
2024-01-26 22:01:06 -0300

    feat: add BPM info and buttons to increase and decrease

--- js/index.js
+++ js/index.js
@@ -1,22 +1,67 @@
 var bpm = 60;
-var intervalId = 1;
+var bpmInMilliseconds = 1000;
 
+var intervalId = 1;
+var isPlaying = false;
+
+
+
+var tempo = 1;
+var tempoMark = 1;
+
+const initialBeat = "beat-1.1";
+
+var lastBeat = initialBeat;
+
+function printTempo() {
+    if (lastBeat) {
+        document.getElementById(lastBeat).className = "deactivated";
+    }
+
+    lastBeat = `beat-${tempo}.${tempoMark}`;
+    document.getElementById(lastBeat).className = "activated";
+
+    tempoMark++;
+    if (tempoMark > 4) {
+        //tempo++;
+        tempoMark = 1;
+    }
+    if (tempo > 4) {
+        tempo = 1;
+    }
+
+    return false;
+}
 
 
 function play() {
-    intervalId = setInterval(printTempo, 150);
+    if (isPlaying) return;
+    isPlaying = true;
+    console.info("Lets Rick n Roll!");
+
+    intervalId = setInterval(printTempo, bpmInMilliseconds);
 }
 
 function stop() {
+    if (!isPlaying) return;
+    isPlaying = false;
+
+    console.info("Pq parou? parou pq?");
     clearInterval(intervalId);
 }
 
 function increaseBpm() {
+    if (bpm >= 300) return;
+
     bpm = bpm + 1;
-    return parseBpmToMilliseconds(bpm);
+    bpmInMilliseconds = parseBpmToMilliseconds(bpm);
+    document.getElementById("actualBpm").textContent = bpm;
 }
 
 function decreaseBpm() {
+    if (bpm <= 1) return;
+
     bpm = bpm - 1;
-    return parseBpmToMilliseconds(bpm);
+    bpmInMilliseconds = parseBpmToMilliseconds(bpm);
+    document.getElementById("actualBpm").textContent = bpm;
 }
